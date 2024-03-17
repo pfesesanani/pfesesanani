f74a15a8738f7d8c3b7a6d4f9f02954ed02f75ef
22009548 <134001178+pfesesanani@users.noreply.github.com>
2024-03-17 15:59:52 +0200

    Add files via upload

---
+++ 22009548spooks/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+
+
+
+ int d;
+  int h;
+   int angle;
+    int output;
+    cout<<"enter height in inches"<<endl;
+     cin>>h;
+     cout<<"enter distance in feet"<<endl;
+      cin>>d; cout<<"ente angle in degrees"<<endl;
+       cin>>angle;
+       output=((h0.883)+(dtan(angle)));
+       cout<<"answer you looking for is:"<<output<<endl;
+
+    return 0;
+}
